ffcf63228ed6e402e8b86c4210db122498d21b51
kengne ruphine <ruphinekengne@gmail.com>
2020-02-12 17:37:02 +0100

    integrating travis ci

--- server.js
+++ server.js
@@ -6,4 +6,7 @@ app.get('/', (req, res) => {
     res.send("Hello World")
 })
 
-app.listen(port, () => console.log(`App is listening on port ${port}`))
+const server = app.listen(port, () =>
+    console.log(`App is listening on port ${port}`));
+
+module.exports = server;
---
+++ test/server.spec.js
@@ -0,0 +1,8 @@
+const expect = require('chai').expect;
+const server = require('../server');
+
+describe('test', () => {
+    it('should return a string', () => {
+        expect('Hello World').to.equal('Hello World');
+    });
+});
